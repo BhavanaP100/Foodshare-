55de2d121624a80a990ba3c75dec511d4652f96b
BhavanaP100 <patilbhavana063@gmail.com>
2026-09-22 16:20:35 +0530

    fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://foodshare-backend-ewq8.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
